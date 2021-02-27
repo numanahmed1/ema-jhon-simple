fba867be0f89876ef334455dd2444d5f07caa218
freelancernuman <numanahmed317@gmail.com>
2021-02-28 03:51:49 +0600

    All interaction added on Ema Jhon App

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = ({ cart }) => {
+  const formatNumber = (num) => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+  };
+  const total = cart.reduce((total, prd) => total + prd.price, 0);
+  const totalPrice = formatNumber(total);
+  //   let totalPrice = 0;
+  //   for (let i = 0; i < cart.length; i++) {
+  //     const product = cart[i];
+  //     totalPrice = totalPrice + product.price;
+  //   }
+
+  let shippingCost = 0;
+  if (totalPrice > 50) {
+    shippingCost = 0;
+  } else if (totalPrice > 20) {
+    shippingCost = 4.99;
+  } else if (totalPrice > 0) {
+    shippingCost = 12.99;
+  }
+
+  const tax = formatNumber(totalPrice / 10);
+  const grandTotal = formatNumber(totalPrice + shippingCost + tax);
+  return (
+    <div className="cart-main">
+      <h3>Order Summery</h3>
+      <h4>Items Ordered: {cart.length}</h4>
+      <h5>Items Price: {totalPrice}</h5>
+      <h5>Shipping Cost: {shippingCost}</h5>
+      <h5>Tax + Vat: {tax}</h5>
+      <h5>Total Price: ${grandTotal}</h5>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,9 +3,8 @@ import "./Product.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-const Product = ({ product }) => {
+const Product = ({ product, handleAddProduct }) => {
   const { name, img, seller, price, stock } = product;
-  console.log(product);
   return (
     <div className="product">
       <div className="product-left">
@@ -18,7 +17,7 @@ const Product = ({ product }) => {
         </p>
         <p>${price}</p>
         <p>only {stock} left in stock - order soon</p>
-        <button className="cart-btn">
+        <button onClick={() => handleAddProduct(product)} className="cart-btn">
           <FontAwesomeIcon className="icon" icon={faShoppingCart} /> Add to cart
         </button>
       </div>
